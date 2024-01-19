79d0a0c7066902db25cb7dfcc344b4fe6ea0f0d9
ridasilva <ridasilva@usp.br>
2024-01-19 17:10:40 -0300

    working celery parameters version

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,14 @@ RUN conda env create -f environment.yml
 RUN echo "source activate formatdb" > ~/.bashrc
 ENV PATH /opt/conda/envs/formatdb/bin:$PATH
 
+RUN pip install --upgrade pip setuptools
+RUN pip install tensorflow
+RUN pip install markupsafe==1.1.1
+RUN pip install werkzeug==0.15.5
+RUN pip install tqdm
+RUN pip install joblib 
+RUN pip install gevent 
+#RUN pip install celery --upgrade
 COPY . .
 
 CMD gunicorn -b 0.0.0.0:8000 --access-logfile - "api.upload:app"
--- api/upload.py
+++ api/upload.py
@@ -2,31 +2,36 @@ from flask import Flask, render_template, request, make_response
 from werkzeug import secure_filename
 import pandas as pd
 from celery import uuid
+#import uuid
 from celery.result import AsyncResult
 import os
 
 from api import task
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/upload')
 def upload_temp():
    return render_template('upload.html')
-		
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       result = request.form
       email = result['email']
+      tool = result['tool']
+      print(tool)
       #file = request.files.get('file')
       #df = pd.read_table(file)
       task_id = uuid()
+      #task_id = str(uuid.uuid4())
       fls = os.path.join('/formatdb_flask/api/tmp', task_id)
       #f.save(secure_filename(fls))
       f.save(fls)
       #print(task_id)
-      df = task.longtask.apply_async(args=[task_id, email], task_id=task_id)
+      df = task.longtask.apply_async(args=[task_id, email, tool], task_id=task_id)
       #df = task.longtask.delay(task_id)
       #print(df.status)
       #print(df.task_id)
@@ -51,4 +56,4 @@ def status(task_id):
 
 if __name__ == '__main__':
     #app.run(debug = True)
-    app.run(host='143.107.202.155')
+    app.run(port=5002, debug = True)
